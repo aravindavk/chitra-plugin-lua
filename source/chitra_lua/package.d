a851a021c6fbd60c249aa3ddcba46889a463074f
Aravinda VK <vkaravinda7@gmail.com>
2026-07-10 14:54:25 +0530

    Added `grid`, `gridCell` and `gridArea`
    Signed-off-by: Aravinda VK <vkaravinda7@gmail.com>

--- source/chitra_lua/package.d
+++ source/chitra_lua/package.d
@@ -9,6 +9,78 @@ import lualibs;
 
 alias LuaState = lua_State*;
 
+int grid(LuaState L)
+{
+    int argsCount = lua_gettop(L);
+    int rows = 1;
+    int cols = 1;
+    double gap = 0.0;
+
+    if (argsCount >= 1 && lua_isinteger(L, 1))
+        cols = cast(int) lua_tointeger(L, 1);
+
+    if (argsCount > 1 && lua_isinteger(L, 2))
+        rows = cast(int) lua_tointeger(L, 2);
+
+    if ((argsCount == 2 && lua_istable(L, 2)) || (argsCount == 3 && lua_istable(L, 3)))
+    {
+        int idx = lua_istable(L, 2) ? 2 : 3;
+        lua_getfield(L, idx, "gap");
+        gap = cast(double) lua_tonumber(L, -1);
+        lua_pop(L, 1);
+    }
+
+    auto ctx = chitraContextFromGlobal(L);
+    ctx.grid(cols, rows, gap: gap);
+
+    return 0;
+}
+
+void returnBox(T)(LuaState L, T box)
+{
+    // 1. Create a new empty table on the stack
+    lua_newtable(L);
+
+    lua_pushstring(L, "x");    // key
+    lua_pushnumber(L, box.x);  // value
+    lua_settable(L, -3);       // Pops key and value, sets them in table
+
+    lua_pushstring(L, "y");
+    lua_pushnumber(L, box.y);
+    lua_settable(L, -3);
+
+    lua_pushstring(L, "width"); 
+    lua_pushnumber(L, box.width);
+    lua_settable(L, -3); 
+
+    lua_pushstring(L, "height");
+    lua_pushnumber(L, box.height);
+    lua_settable(L, -3);
+}
+
+int gridCell(LuaState L)
+{
+    int idx = cast(int) lua_tointeger(L, 1);
+
+    auto ctx = chitraContextFromGlobal(L);
+    auto box = ctx.gridCell(idx);
+    returnBox(L, box);
+
+    return 1;
+}
+
+int gridArea(LuaState L)
+{
+    int startNumber = cast(int) lua_tointeger(L, 1);
+    int endNumber = cast(int) lua_tointeger(L, 2);
+
+    auto ctx = chitraContextFromGlobal(L);
+    auto box = ctx.gridArea(startNumber, endNumber);
+    returnBox(L, box);
+
+    return 1;
+}
+
 int size(LuaState L)
 {
     int argsCount = lua_gettop(L);
@@ -76,6 +148,9 @@ void fromLuaString(string code, string output = "")
 
     lua_register(L, "size", &size);
     lua_register(L, "save", &saveAs);
+    lua_register(L, "grid", &grid);
+    lua_register(L, "gridCell", &gridCell);
+    lua_register(L, "gridArea", &gridArea);
 
     auto ret = luaL_dostring(L, code.toStringz);
 
